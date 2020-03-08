36398d6c6daac470c1cd5bf6cf85d6184dfb6c37
Simranjit Kaur Maan <simranjitkaur94mec@gmail.com>
2020-03-08 16:53:41 +0530

    changed modal content

--- src/shared/Modal.js
+++ src/shared/Modal.js
@@ -4,16 +4,6 @@ import ReactDOM from "react-dom";
 const Modal = props => {
   if (props.meal == null) return <div>Loading...</div>;
 
-  var ingredients = [];
-  var measures = [];
-  Object.keys(props.meal).forEach(function(key) {
-    if (key.includes("strIngredient") && props.meal[key] != "")
-      ingredients.push(props.meal[key]);
-
-    if (key.includes("strMeasure") && props.meal[key] != "")
-      measures.push(props.meal[key]);
-  });
-
   return ReactDOM.createPortal(
     <div
       onClick={() => {
