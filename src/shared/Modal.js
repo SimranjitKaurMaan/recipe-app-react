87123d30882d87fbffb5cc184df478918852591a
Simranjit Kaur Maan <simranjitkaur94mec@gmail.com>
2020-03-08 16:52:17 +0530

    added a header

--- src/shared/Modal.js
+++ src/shared/Modal.js
@@ -4,9 +4,21 @@ import ReactDOM from "react-dom";
 const Modal = props => {
   if (props.meal == null) return <div>Loading...</div>;
 
+  var ingredients = [];
+  var measures = [];
+  Object.keys(props.meal).forEach(function(key) {
+    if (key.includes("strIngredient") && props.meal[key] != "")
+      ingredients.push(props.meal[key]);
+
+    if (key.includes("strMeasure") && props.meal[key] != "")
+      measures.push(props.meal[key]);
+  });
+
   return ReactDOM.createPortal(
     <div
-      onClick={()=>{props.onDismiss()}}
+      onClick={() => {
+        props.onDismiss();
+      }}
       className={
         "ui page modals dimmer top aligned visible " +
         (props.show ? "active" : "hidden")
@@ -23,6 +35,7 @@ const Modal = props => {
           <img class="ui image medium" src={props.meal.strMealThumb} />
         </div>
         <div className="header">{props.meal.strMeal}</div>
+        <h4 class="header">Instructions</h4>
         <div className="content">{props.meal.strInstructions}</div>
       </div>
     </div>,
