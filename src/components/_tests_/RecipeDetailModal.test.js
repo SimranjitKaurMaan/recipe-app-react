3e746bb425f8feffb18e14100ce61f17285ff72f
Simranjit Kaur Maan <simranjitkaur94mec@gmail.com>
2020-03-10 08:52:35 +0530

    changed unit tests

--- src/components/_tests_/RecipeDetailModal.test.js
+++ src/components/_tests_/RecipeDetailModal.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { mount , configure} from 'enzyme';
+import { mount , shallow,configure} from 'enzyme';
 import ReactDOM from 'react-dom';
 import Adapter from 'enzyme-adapter-react-16';
 import App from '../App';
@@ -10,10 +10,8 @@ configure({adapter: new Adapter()});
 
 it('shows a recipe image and text ', () =>
 {
-//     const wrapped = mount(<RecipeDetailModal show='true' meal={[{"idMeal":"52926","strMeal":"Tourtiere"}]}/>);
-
-//    console.log(wrapped.find('div'));
-
+    const wrapped = mount(<RecipeDetailModal show='true'/>);
+    expect(wrapped.find('div').length).toEqual(1);
 
 });
 
